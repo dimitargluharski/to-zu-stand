f841133b08e49454c09dd0e4afb885cd77c76d83
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-10-11 00:07:58 +0300

    implements remove func

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
 import { Todo } from "./components/Todo";
 import { useStore } from "./store/todosStore";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todoString, setTodoString] = useState<string>('');
 
   const todos = useStore((state) => state.todos);
   const addTodo = useStore((state) => state.addTodo);
+  const removeTodo = useStore((state) => state.removeTodo);
 
   const handleAddNewTodo = () => {
     if (todoString === '') return;
 
-    addTodo('1', todoString); // change hardcoded 1
+    addTodo(uuidv4(), todoString); // change hardcoded 1
     setTodoString('');
   }
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTodoString(event.target.value);
+  }
+
+  const handleRemoveTodo = (id: string) => {
+    removeTodo(id);
   }
 
   return (
@@ -30,7 +36,7 @@ function App() {
       </button>
 
       <div>
-        {todos.length > 0 ? todos.slice().reverse().map((todo, index) => <Todo key={index} {...todo} />) : <p>There are no todos, but you can add one!</p>}
+        {todos.length > 0 ? todos.slice().reverse().map((todo) => <Todo id={todo.id} handleRemoveTodo={handleRemoveTodo} key={todo.id} message={todo.message} />) : <p>There are no todos, but you can add one!</p>}
       </div>
 
     </>
