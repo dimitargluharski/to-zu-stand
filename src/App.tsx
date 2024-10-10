76a43b5b7c0c97c1736c2616b6fa8a25f2131f75
Dimitar Gluharski <dimitargluharski@gmail.com>
2024-10-10 23:18:24 +0300

    create basic actions

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,8 @@ function App() {
   const todos = useStore((state) => state.todos);
   const addTodo = useStore((state) => state.addTodo);
 
-  const LOREM_TEXT = 'LOREM IPSUM DOLOR SIT AMET!';
-
-  const handleAddTodo = () => {
-    addTodo(LOREM_TEXT);
-  };
-
   return (
-    <>
-      {todos.map((todo, index) => (
-        <div key={index}>{todo}</div>
-      ))}
-      <button onClick={handleAddTodo}>Add Todo</button>
-    </>
+    <h1>Zustand Todo App</h1>
   );
 }
 
